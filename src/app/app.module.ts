176570d47377af17f9a5734f4267bc3d274a8eaa
vsavkin <avix1000@gmail.com>
2018-02-04 17:55:49 -0500

    ngrx-serializer-repro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {ActivatedRouteSnapshot, RouterModule, RouterStateSnapshot} from '@angular/router';
+import {Store, StoreModule} from '@ngrx/store';
+import {StoreRouterConnectingModule, routerReducer, RouterStateSerializer} from '@ngrx/router-store';
+import {storeFreeze} from 'ngrx-store-freeze';
 
+@Component({
+  selector: 'app-aa',
+  template: 'aa <a routerLink="/bb">bb</a>'
+})
+export class ComponentA {}
+
+@Component({
+  selector: 'app-bb',
+  template: 'bb <a routerLink="/aa">aa</a>'
+})
+export class ComponentB {}
+
+export class CustomSerializer implements RouterStateSerializer<any> {
+  serialize(routerState: RouterStateSnapshot): any {
+    return {root: this.serializeRoute(routerState.root), url: routerState.url};
+  }
+
+  private serializeRoute(route: ActivatedRouteSnapshot) {
+    const children = route.children.map(c => this.serializeRoute(c));
+    return {params: route.params, data: route.data, url: route.url, outlet: route.outlet, routeConfig: route.routeConfig, children};
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentA,
+    ComponentB
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'aa'},
+      { path: 'aa', component: ComponentA },
+      { path: 'bb', component: ComponentB }
+    ]),
+    StoreRouterConnectingModule,
+    StoreModule.forRoot({router: routerReducer}, {metaReducers: [storeFreeze]})
   ],
-  providers: [],
+  providers: [
+    {provide: RouterStateSerializer, useClass: CustomSerializer}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(s: Store<any>) {
+    s.subscribe(state => console.log(JSON.stringify(state)));
+  }
+}
